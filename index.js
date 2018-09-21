cd0f47420ca770818482d4cebfdec19359ce1d3b
Ryan Cole <ryan@rycole.com>
2018-09-20 19:59:33 -0500

    use puppeteer to control browser - hit wall with captcha, unsure if mouse can be used to get around it

--- index.js
+++ index.js
@@ -15,14 +15,28 @@ async function login() {
   await page.goto("https://www.twitch.tv/login");
 
   // write in our username and password
-  await page.type("div[data-a-target='login-username-input'] input", "test");
-  await page.type("div[data-a-target='login-password-input'] input", "test");
+  await page.type("div[data-a-target='login-username-input'] input", "foo");
+  await page.type("div[data-a-target='login-password-input'] input", "bar");
 
   // press the login button
   await page.click("button[data-a-target='passport-login-button']");
 
   // submit the form
   await page.click("button[data-a-target='passport-login-button']")
+
+  // wait for captcha to appear
+  await page.waitForSelector("#recaptcha-element-container iframe");
+
+  const captchaContainer = await page.$("#recaptcha-element-container");
+
+  // get the coordinates of the captcha container
+  const coordinates = await page.evaluate(el => {
+    const {top, left, bottom, right} = el.getBoundingClientRect();
+	return {top, left, bottom, right};
+  }, captchaContainer);
+
+  // move mouse to the captcha box
+  // await page.
 
   await page.screenshot({path: 'example.png'});
 }
@@ -33,3 +47,6 @@ async function main() {
 }
 
 main();
+
+
+// NOTE TO SELF: make workflow be: open page to logins/new page, if redirect occurs we need to login so perform login for that execution, otherwise perform username checks - loop around that workflow
